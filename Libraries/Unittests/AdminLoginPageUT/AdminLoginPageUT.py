94ef53cf918f60a93fef0e0ed4cc3f08efeea304
TeddyTeddy <hakan.cuzdan@yandex.com>
2020-02-18 16:23:16 +0200

    test_login test implemented

--- Libraries/Unittests/AdminLoginPageUT/AdminLoginPageUT.py
+++ Libraries/Unittests/AdminLoginPageUT/AdminLoginPageUT.py
@@ -13,10 +13,21 @@ class AdminLoginPageUT(unittest.TestCase):
         self._admin_login_page = AdminLoginPage()
 
     def test_login(self):
+        # function under test gets called
         self._admin_login_page.login(DICT__CREDENTIALS['valid_admin']['username'],
                                      DICT__CREDENTIALS['valid_admin']['password'])
-        verify(LibraryLoader.get_instance().sl,
-               times=1).input_text(locator=locator['admin_login_page']['username_field'], text='hakan')
+
+        # verify that correct mock instance sl got called with correct instance methods with correct arguments
+        verify(LibraryLoader.get_instance().sl, times=1).input_text(
+            locator=locator['admin_login_page']['username_field'],
+            text=DICT__CREDENTIALS['valid_admin']['username'])
+
+        verify(LibraryLoader.get_instance().sl, times=1).input_password(
+            locator=locator['admin_login_page']['password_field'],
+            password=DICT__CREDENTIALS['valid_admin']['password'])
+
+        verify(LibraryLoader.get_instance().sl, times=1).click_element(
+            locator=locator['admin_login_page']['login_button'])
 
     def tearDown(self) -> None:
         unstub()
