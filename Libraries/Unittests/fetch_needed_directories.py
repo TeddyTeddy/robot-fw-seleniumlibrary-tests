fac3f67562fa907d842d05645b0d464a4c6e31fe
TeddyTeddy <hakan.cuzdan@yandex.com>
2020-02-19 09:21:37 +0200

    fetch_needed_directories.py now checks for the needed directories b4 adding them to PYTHONPATH

--- Libraries/Unittests/fetch_needed_directories.py
+++ Libraries/Unittests/fetch_needed_directories.py
@@ -8,6 +8,15 @@ if __name__ == '__main__':
     src_abs_path = m.groups()[0] + '/Src/'               # forms src_abs_path from the grabbed string
     unittests_common_abs_path = current_abs_path + '/Common/'
     result = src_abs_path + ':' + unittests_common_abs_path
+    if 'PYTHONPATH' in os.environ:
+        if result in os.environ['PYTHONPATH']:
+            # if result already is included in PYTHONPATH, use PYTHONPATH as it is
+            result = os.environ['PYTHONPATH']
+        else:
+            # if result is not included in PYTHONPATH, then result will extend PYTHONPATH
+            result = os.environ['PYTHONPATH'] + ':' + result
+    else:
+        pass
     file = open('needed_abs_directories.txt', 'w+')
     file.write(result)
     file.close()
