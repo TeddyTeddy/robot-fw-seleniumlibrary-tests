bfcfc2f7bba0fab1889e958cae93b755b80a1127
TeddyTeddy <hakan.cuzdan@yandex.com>
2020-03-01 11:17:53 +0200

    LibraryLoaderStub.py : added comments on spec=LibraryLoader

--- Libraries/Unittests/Common/LibraryLoaderStub.py
+++ Libraries/Unittests/Common/LibraryLoaderStub.py
@@ -6,6 +6,9 @@ def configure_mock_library_loader():
     # sl stands for selenium library
     sl = mock(strict=True)     # every un-configured, unexpected call on sl will raise an exception
 
+    # You can also create an empty Mock which is specced against a given spec: spec=LibraryLoader.
+    # These mock are by default strict, thus they raise if you want to stub a method, the spec does not implement.
+    # Mockito will also match the function signature.
     ll = mock({
         'sl': sl,
     }, spec=LibraryLoader)
